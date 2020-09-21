53cd81ff9295399521aea38164dd14770617635c
Alejandro Rodriguez <alejandro.rodriguez@patelhs.org>
2020-09-21 15:43:22 +0000

    I added 3 int to the code

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class  {
   public static void main(String[] args) {
-  
+  int myAge = 14;
+  int myBirthday = 0628;
+  int todaysDate = 0921;
+
+  if(myBirthday == todaysDate)
+  { 
+    myAge = 15;
+    System.out.print("I am " + myAge + " years old");
+   }else{
+    System.out.print(" I am still " + myAge + " years old");
+   }
   }
 }
